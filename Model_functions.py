040b16085640455bfcca34aa435897d0278af289
Jaco-hub <84577328+Jaco-hub@users.noreply.github.com>
2021-05-27 16:12:46 +0200

    Add files via upload

---
+++ Model_functions.py
@@ -0,0 +1,164 @@
+# All functions needed to train the model are here.
+
+# Import the packages needed
+from os import truncate
+import numpy as np
+import json
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torchvision
+from torchvision import datasets, transforms, models
+from collections import OrderedDict
+
+def data_load(data_dir):
+    # Loads the data from the indicated location (data_dir) and then applies the transformation to the images.
+    # Outputs are the dataloaders
+
+    # Directories where the data is stored
+    train_dir = data_dir + '/train'
+    valid_dir = data_dir + '/valid'
+    test_dir = data_dir + '/test'
+
+    # Define your transforms for the training, validation, and testing sets
+    train_transforms = transforms.Compose([transforms.RandomRotation(25),
+                                           transforms.RandomResizedCrop(224),
+                                           transforms.RandomHorizontalFlip(),
+                                           transforms.ToTensor(),
+                                           transforms.Normalize([0.485, 0.456, 0.406], 
+                                                                [0.229, 0.224, 0.225])])
+
+    valid_test_transforms = transforms.Compose([transforms.Resize(256),
+                                           transforms.CenterCrop(224),
+                                           transforms.ToTensor(),
+                                           transforms.Normalize([0.485, 0.456, 0.406], 
+                                                                [0.229, 0.224, 0.225])])
+
+    # Load the datasets with ImageFolder
+    # image_datasets = 
+    train_data = datasets.ImageFolder(train_dir, transform=train_transforms)
+    valid_data = datasets.ImageFolder(valid_dir, transform=valid_test_transforms)
+    test_data = datasets.ImageFolder(test_dir, transform=valid_test_transforms)
+
+    # Using the image datasets and the trainforms, define the dataloaders
+    trainloader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True)
+    validloader = torch.utils.data.DataLoader(valid_data, batch_size=32, shuffle=False)
+    testloader = torch.utils.data.DataLoader(test_data, batch_size=32, shuffle=False)
+
+    return trainloader, validloader, testloader
+
+def load_mapping(cat_name):
+    # Loads the mapping of the category numbers to category names
+    # file name is taken from argparse
+    with open('cat_name', 'r') as f:
+        cat_to_name = json.load(f)
+
+    return cat_to_name
+
+
+def gpu_cpu(gpu):
+    # Using the GPU (Cuda) or CPU to train
+    if gpu == True:
+        device = torch.device ("cuda" if torch.cuda.is_available() else "cpu")
+    else:
+        device = 'cpu'
+
+    return device
+
+
+def load_network(arch):
+    # load a pre-trained netwerk selected
+    model = models.arch(pretrained=True)
+
+    # Freeze parameters so we don't backprop throu,
+    for param in model.parameters():
+        param.requires_grad = False
+
+    return model
+
+
+def classifier(hidden_units):
+    # Define new classifier
+    classifier = nn.Sequential(OrderedDict([
+                              ('fc1', nn.Linear(25088, hidden_units, bias=True)),
+                              ('relu1', nn.ReLU()),
+                              ('dropout1', nn.Dropout(p=0.5)),
+                              ('fc2', nn.Linear(hidden_units, 102)),
+                              ('output', nn.LogSoftmax(dim=1))
+                          ]))
+    #model.classifier = classifier
+
+    return classifier
+
+
+def training(model, classifier, learning_rate, device, epochs, trainloader, validloader):
+    #
+    # Setting up the training configurations and definitions
+    # # Loss with the Negative Log LikeLihood
+    criterion = nn.NLLLoss()
+    # learning_rate = 0.001
+    # Adam is the chosen Optimizer
+    optimizer = optim.Adam(model.classifier.parameters(), lr=learning_rate)
+    # Put the model into the GPU
+    model.to(device)
+
+    # Define the training parameters
+    steps = 0
+    print_every = 50
+
+    # Let's start training!
+    for ii in range(epochs):
+        train_loss = 0
+        for images, labels in trainloader:
+            steps += 1
+
+            # Move image and label tensors to the GPU
+            images, labels = images.to(device), labels.to(device)    
+    
+            #Zeroing the gradients
+            optimizer.zero_grad()
+
+            #Forward pass
+            output = model.forward(images)
+            loss = criterion(output, labels)
+    
+            # Backward propagation
+            loss.backward()
+            optimizer.step()
+        
+            train_loss += loss.item()
+        
+            #validation
+            if steps % print_every ==0:
+                accuracy = 0
+                valid_loss = 0
+                model.eval()
+
+                # Turn off gradients for validation, saves memory and computations
+                with torch.no_grad():
+                    for images, labels in validloader:
+                        #move the data to the gpu
+                        images, labels = images.to(device), labels.to(device)
+
+                        #forward pass with the validation data      
+                        output = model.forward(images)
+                        valid_loss += criterion(output, labels).item()
+
+                        #accuracy
+                        ps = torch.exp(output)
+                        top_prob, top_class = ps.topk(1, dim=1)
+                        equality = top_class == labels.view(*top_class.shape)
+                        accuracy += equality.type(torch.FloatTensor).mean()
+        
+                print("Epoch: {}/{}.. ".format(ii+1, epochs),
+                      "Training Loss: {:.3f}.. ".format(train_loss/print_every),
+                      "Valid Loss: {:.3f}.. ".format(valid_loss/len(validloader)),
+                      "Valid Accuracy: {:.3f}".format(accuracy/len(validloader)))
+        
+                train_loss = 0
+                # Make sure training is back on to allow drop out
+                model.train()
+
+    return model
---
+++ Train.py
@@ -0,0 +1,36 @@
+# This function retries the inputs from the user using Argparse module for Python.
+# To ensure that the process runs, default values will be used in case any inputs are missing.
+# for useful info on argparse module, go to: https://www.golinuxcloud.com/python-argparse/
+# import packages
+import argparse
+
+from torchvision import models
+from Model_funtions import data_load, gpu_cpu, load_network, new_classifier, training
+
+# def get_input_args():
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir', default = 'flowers', dest = 'data_dir', help= 'Path to the folder with data')
+    #parser.add_argument('--save_dir', default = opzoeken in de boom help = 'Where to save the checkpoint')
+parser.add_argument('--cat_name', default = 'cat_to_name.json', dest = 'cat_name', help = 'Name of the file mapping the category numbers to names')
+parser.add_argument('--arch', default = 'vgg13', dest = 'arch', help = 'Chose the model to be used, options are vgg11, vgg13, vgg16 and vgg19')
+parser.add_argument('--learning_rate', default = 0.01, type = float, dest= 'learning_rate', help = 'Set the learning rate of the model')
+parser.add_argument('--epochs', default = 2, type = int, dest = 'epochs', help = 'Set the number of epochs to train the model')
+parser.add_argument('--hidden_units', default = 512, type = int, dest = 'hidden_units', help = 'Set the number of hidden units in the classifier')
+parser.add_argument('--gpu', type = bool, default = True, help = 'Use the GPU or cpu?')
+args = parser.parse_args()
+
+
+
+# run the training by executing the functions in model_functions
+# load the data and create the datasets
+trainloader, validloader, testloader = data_load(args.data_dir)
+# using the GPU?
+device = gpu_cpu(args.gpu)
+# 
+model = load_network(args.arch)
+#
+classifier = new_classifier(args.hidden_units)
+# returns the trained model
+t_model = training(model, classifier, args.learning_rate, device, args.epochs, trainloader, validloader)
+
+
